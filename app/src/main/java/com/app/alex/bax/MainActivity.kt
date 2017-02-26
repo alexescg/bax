ba5277f7c9b5347bc16db23ae446544bc46eeae9
alex <aeg0204@gmail.com>
2017-02-26 15:07:53 -0700

    Added Timer View

--- app/src/main/java/com/app/alex/bax/MainActivity.kt
+++ app/src/main/java/com/app/alex/bax/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.app.alex.bax
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initListeners()
     }
+
+    fun initListeners() {
+        btn_timer.setOnClickListener {
+            createTimerIntent()
+        }
+    }
+
+    fun createTimerIntent() {
+        val timerIntent: Intent = Intent(baseContext, TimerActivity::class.java)
+        startActivity(timerIntent)
+    }
+
+
 }
---
+++ app/src/main/java/com/app/alex/bax/TimerActivity.kt
@@ -0,0 +1,13 @@
+package com.app.alex.bax
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class TimerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_timer)
+
+    }
+}
